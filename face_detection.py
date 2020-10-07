ae7457a071b0b2cceab3b6df5c4609f16ed068d9
Babitababy <61555001+Babitababy@users.noreply.github.com>
2020-10-07 16:28:37 +0530

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -1,7 +1,6 @@
 import cv2
 
 face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-#img=cv2.imread('D:\only memories/babbi.jpg')
 cap=cv2.VideoCapture(0)
 
 while cap.isOpened():
